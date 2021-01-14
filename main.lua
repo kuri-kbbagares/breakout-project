9284c41ece8392d22e9a89c1c7428c9a5c19ef58
shaina-espulgar <72699933+shaina-espulgar@users.noreply.github.com>
2021-01-14 16:33:48 +0800

    Updated for the movement

--- main.lua
+++ main.lua
@@ -7,20 +7,32 @@ VIRTUAL_WIDTH = 432
 VIRTUAL_HEIGHT = 243
 
 function love.load()
-    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
-        vsync = true,
-        fullscreen = false
-    })
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+    
+    math.randomseed(os.time())
     
     gSprites = {
         ['background'] = love.graphics.newImage('sprites/background.png'),
         ['blocks'] = love.graphics.newImage('sprites/blocks.png'),
         ['hearts'] = love.graphics.newImage('sprites/hearts.png')
     }
+    
+    gFrames = {
+        ['paddles'] = GenerateQuadsPaddles(gTextures['main']),
 
+    }
+    
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+        vsync = true,
+        fullscreen = false
+    })
+    
     gStateMachine = {
+        ['start'] = function() return StartState() end,
+        ['serve'] = function() return ServeState() end,
         ['menu'] = function() return MenuState() end,
-        ['play'] = function() return PlayState() end
+        ['play'] = function() return PlayState() end,
+        ['paddle-select'] = function() return PaddleSelectState() end
     }
 
     gFrames = {
@@ -29,9 +41,24 @@ function love.load()
 end
 
 function love.update()
-
+    
+    gStateMachine:update(dt)
+    love.keyboard.keysPressed = {}
+    
 end
 
+function love.keypressed(key)
+    -- add to our table of keys pressed this frame
+    love.keyboard.keysPressed[key] = true
+end
+
+function love.keyboard.wasPressed(key)
+    if love.keyboard.keysPressed[key] then
+        return true
+    else
+        return false
+    end
+end
 
 
 function love.draw()
@@ -43,6 +70,8 @@ function love.draw()
     love.graphics.draw(gSprites['background'], 0, 0, 0, VIRTUAL_WIDTH / (backgroundWidth - 1), VIRTUAL_HEIGHT / (backgroundHeight - 1))
 
     love.graphics.draw(gSprites['blocks'], gFrames['paddle'], VIRTUAL_WIDTH / 2 - 32, VIRTUAL_HEIGHT - VIRTUAL_HEIGHT / 10)
+    
+    gStateMachine:render()
     
     push:apply('end')
 end
