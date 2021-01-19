245a14bb4eb9cbb2a431f55bc5f8b4b99bebf378
Mark Anthony Pandan <72681401+Strygel@users.noreply.github.com>
2021-01-19 22:29:57 +0800

    Minor Fixes
    - Since we only have 4 bricks compared CS50's 25+ ones, I modified the codes.. 
    - Added sounds on the code

--- main.lua
+++ main.lua
@@ -16,9 +16,9 @@ function love.load()
     love.window.setTitle('Breakout Project')
     
     gFonts = {
-      ['smallFont'] = love.graphics.newFont('font/font.ttf', 8),
-      ['mediumFont'] = love.graphics.newFont('font/font.ttf', 16),
-      ['largeFont'] = love.graphics.newFont('font/font.ttf', 32)
+      ['small'] = love.graphics.newFont('font/font.ttf', 8),
+      ['medium'] = love.graphics.newFont('font/font.ttf', 16),
+      ['large'] = love.graphics.newFont('font/font.ttf', 32)
       }
     
     gSprites = {
@@ -36,6 +36,24 @@ function love.load()
         --needs update
         --['particle'] = love.graphics.newImage('graphics/particle.png')
     }
+
+    gSounds = {
+      ['paddle-hit'] = love.audio.newSource('sounds/paddle_hit.wav', 'static'),
+      ['score'] = love.audio.newSource('sounds/score.wav', 'static'),
+      ['wall-hit'] = love.audio.newSource('sounds/wall_hit.wav', 'static'),
+      ['confirm'] = love.audio.newSource('sounds/confirm.wav', 'static'),
+      ['select'] = love.audio.newSource('sounds/select.wav', 'static'),
+      ['no-select'] = love.audio.newSource('sounds/no-select.wav', 'static'),
+      ['brick-hit-1'] = love.audio.newSource('sounds/brick-hit-1.wav', 'static'),
+      ['brick-hit-2'] = love.audio.newSource('sounds/brick-hit-2.wav', 'static'),
+      ['hurt'] = love.audio.newSource('sounds/hurt.wav', 'static'),
+      ['victory'] = love.audio.newSource('sounds/victory.wav', 'static'),
+      ['recover'] = love.audio.newSource('sounds/recover.wav', 'static'),
+      ['high-score'] = love.audio.newSource('sounds/high_score.wav', 'static'),
+      ['pause'] = love.audio.newSource('sounds/pause.wav', 'static'),
+
+      ['music'] = love.audio.newSource('sounds/music.wav', 'stream')
+  }
 
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         vsync = true,
